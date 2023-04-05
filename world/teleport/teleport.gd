594b06998b1f1707ff20ec08e8d88ba4d8a38eba
Breadp4ck <iii103@mail.ru>
2023-04-05 21:08:42 +0700

    #14 Change bricks, #13 Add collisions for hub, #12 Add hub and the first room

--- world/teleport/teleport.gd
+++ world/teleport/teleport.gd
@@ -1,7 +1,13 @@
 extends Node
 
-@export var destination_node: Node3D
+@export var destination_marker: Marker3D
+
 
 func _on_area_entered(area: Area3D) -> void:
 	if area.owner is Player:
-		area.owner.global_position = destination_node.global_position
+		area.owner.global_position = destination_marker.global_position
+
+
+func _on_area_exited(area: Area3D) -> void:
+	if area.owner is Player:
+		area.owner.global_position = destination_marker.global_position
