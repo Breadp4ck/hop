22b0f6cd9760868f2ec78695a206878c560fb5fa
Tombleron <fokindacer@gmail.com>
2023-04-06 22:32:39 +0700

    #22 Spell animations

--- world/items/tree/tree.gd
+++ world/items/tree/tree.gd
@@ -3,8 +3,7 @@ extends Item
 @onready var body: StaticBody3D = $StaticBody3D
 
 func interact(spell: Spell) -> void:
-	animator.play("") # Norm anim nado.
-	disable_body() # Remove
+	animator.play("burn") 
 	
-func disable_body() -> void: # Invoke this at and of animation
+func disable_body() -> void:
 	body.queue_free()
