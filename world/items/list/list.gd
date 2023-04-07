873a5d66075225f62c6dc25e633e3a360470db14
twink76 <super.irums2@gmail.com>
2023-04-07 20:30:25 +0700

    Fix bookUI. Add Bricks, Nailed wood. Add List to add spells

--- world/items/list/list.gd
+++ world/items/list/list.gd
@@ -1,17 +1,22 @@
 class_name List
 extends Item
 
+@export var pickable_spell_type: Globals.SpellType = -1
 @export var title: String
 @export var text: String
 
 func interact(spell: Spell) -> void:
 	var book_ui: BookUI = get_tree().get_nodes_in_group("BookUI")[0]
+		
+	if pickable_spell_type != -1:
+		var spell_caster: SpellCaster = get_tree().get_nodes_in_group("SpellCaster")[0]
+		spell_caster.add_spell(pickable_spell_type)
 	
 	var book_texts: Array = []
-	for i in book_ui.list_sequence.size():
-		book_texts.push_back(book_ui.list_sequence[i])
-		
-	if (book_texts.has(text) == false):
-		book_ui.add_page(title, text)
+	for page in book_ui.list_sequence:
+		if page["title"] == title:
+			queue_free()
+			return
 	
+	book_ui.add_page(title, text)
 	queue_free()
